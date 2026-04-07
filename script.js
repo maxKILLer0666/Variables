4e359a36f40d0fd659dd5ef4af57c6f45e768c33
maxKILLer0666 <maxim8777000@gmail.com>
2026-04-07 17:58:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let pon = 12
+console.log(pon)
+
+pon = 23
+console.log(pon)
+
+/*const pom = 13
+console.log(pom)
+pom = 34
+console.log(pom)*/ 
+
+var pomni = 'TADC'
+console.log(pomni)
+
+var pomni = 'УЦЦ'
+console.log(pomni) //var в JavaScript — это ключевое слово для объявления переменных с функциональной или глобальной областью видимости. var устарел в JavaScript из-за отсутствия блочной области видимости.
+//звук в конференции вырублен
+
+let integer = 42;
+console.log(integer)
+let float = 3.14;
+console.log(float)
+let negative = -7;
+console.log(negative)
+let singleQuote = 'Hello'
+console.log(singleQuote)
+let doubleQuote = "World"
+console.log(doubleQuote)
+let templateLiteral = `Hello, ${singleQuote}!`
+console.log(templateLiteral)
+let isTrue = true;
+console.log(isTrue)
+let isFalse = false;
+console.log(isFalse)
+let notANumber = NaN;
+console.log(notANumber)
+let invalidOperation = 0 / 0;
+console.log(invalidOperation)
+let user = null;
+console.log(user);
+let undefinedValue;
+console.error(undefinedValue); 
+let num = 148;
+let strNum = String(num)
+console.log(strNum)
+let ldkl = null
+let Strldkl = String(ldkl)
+console.log(Strldkl)
